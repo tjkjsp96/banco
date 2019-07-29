cf8efa8c2240ea0208730f131ca97da69c7a3f72
prietoisa <prietoisabele@gmail.com>
2019-07-29 11:56:23 -0300

    Finalizando banco

--- banco/urls.py
+++ banco/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from contas.views import mostrar_formulario_cadastro, mostrar_pessoas
+from contas.views import mostrar_formulario_cadastro, mostrar_pessoas, login, cadastrar_conta
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', mostrar_formulario_cadastro),
     path('pessoas', mostrar_pessoas),
+    path('login', login),
+    path('conta', cadastrar_conta)
 ]
